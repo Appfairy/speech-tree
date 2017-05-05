9ae9345c6840e9c35cb4b3a7ef09629b48c851b3
DAB0mB <emanor6@gmail.com>
2017-05-05 18:13:27 -0200

    Splited express middleware into 2

--- server/express.js
+++ server/express.js
@@ -1,49 +1,72 @@
 const express = require('express');
 const createSpeechClassifier = require('./speech_classifier');
 
-function createMiddleware(options) {
+let classifier;
+
+// Options:
+// - trainer (Function): Required. A function which accepts the classifier in-order
+//   to train it.
+// - cacheFile (String): The path for the classifier's cache file.
+// - forceTraining (Boolean): Force the training process even though a cache file is
+//   available.
+function trainClassifier(options) {
+  if (!options.trainer) {
+    throw TypeError('a trainer function must be specified');
+  }
+
+  return (req, res, next) => {
+    if (classifier) return next();
+
+    classifier = createSpeechClassifier();
+
+    options.trainer(classifier).then(() => {
+      if (classifier._trained) {
+        next();
+      }
+      else {
+        next(Error('classifier must be trained'));
+      }
+    });
+  };
+}
+
+// Options:
+// - url (String): The url of the registered route
+function matchLabel(options) {
   // Apply defaults to options
   options = Object.assign({
     url: '/speech-tree/label'
   }, options);
 
-  if (!options.trainer) {
-    throw TypeError('a trainer function must be specified');
-  }
+  const router = express.Router();
 
-  const classifier = createSpeechClassifier(options);
-
-  // Train classifier
-  return options.trainer(classifier).then(() => {
-    if (!classifier._trained) {
-      throw Error('classifier must be trained');
+  router.get(options.url, (req, res) => {
+    if (!classifier) {
+      return next(Error('classifier must be trained'));
     }
 
-    // After we've finished initializing all our logic, we finally return the middleware
-    return (req, res, next) => {
-      if (req.method != 'get') return next();
-      if (req.url != options.url) return next();
+    const sentence = req.query.sentence;
 
-      const sentence = req.query.sentence;
+    if (!sentence) {
+      res.status(400);
+      return res.send('sentence must be provided');
+    }
 
-      if (!sentence) {
-        res.status(400);
-        return res.send('sentence must be provided');
-      }
+    res.status(200);
+    res.setHeader('Content-Type', 'application/json');
 
-      res.status(200);
-      res.setHeader('Content-Type', 'application/json');
-
-      // Classifier might not be trained for some sentences
-      try {
-        const label = classifier.classify(sentence);
-        res.send(JSON.stringify({ label }));
-      }
-      catch (err) {
-        res.send(JSON.stringify({ label: 'notTrained' }));
-      }
-    };
+    // Classifier might not be trained for some sentences
+    try {
+      const label = classifier.classify(sentence);
+      res.send(JSON.stringify({ label }));
+    }
+    catch (err) {
+      res.send(JSON.stringify({ label: 'notTrained' }));
+    }
   });
-}
+};
 
-module.exports = createMiddleware;
+module.exports = {
+  trainClassifier,
+  matchLabel
+};
