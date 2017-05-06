1a0774da769ff0e737d452f1d55250ec0621052e
DAB0mB <emanor6@gmail.com>
2017-05-06 01:20:41 -0200

    Export annyang methods

--- src/client/index.js
+++ src/client/index.js
@@ -1,13 +1,21 @@
+import annyang from 'annyang';
 import labelMatcher from './label_matcher';
 import SpeechNode from './speech_node';
 
 const rootSpeechNode = new SpeechNode();
 
+// Export annyang methods
+Object.keys(annyang)
+  .filter((k) => typeof annyang[k] == 'function')
+  .forEach((k) => rootSpeechNode[k] = annyang[k].bind(annyang));
+
+// Export label matcher methods
 Object.assign(rootSpeechNode, {
   enableLabelMatching: labelMatcher.enableLabelMatching.bind(labelMatcher),
   disableLabelMatching: labelMatcher.disableLabelMatching.bind(labelMatcher),
 });
 
+// Export getters and setters as native properties
 Object.defineProperty(rootSpeechNode, 'labelURL', {
   configurable: true,
   enumerable: true,
