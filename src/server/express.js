325f9b2fd6a01a6407602e5b0bd41ca848142bc6
DAB0mB <emanor6@gmail.com>
2017-05-08 16:56:59 -0200

    Make classifier a generic function in express middleware

--- src/server/express.js
+++ src/server/express.js
@@ -1,63 +1,28 @@
 import express from 'express';
-import natural from 'natural';
-import { LABEL_DEFAULT_ENDPOINT, LABEL_NOT_FOUND } from '../consts';
+import { LABEL_DEFAULT_ENDPOINT } from '../consts';
 
-let classifier;
+function middleware(options) {
+  // Apply defaults to options
+  options = Object.assign({
+    url: LABEL_DEFAULT_ENDPOINT
+  }, options);
 
-// First, we need to initialize the classifier
-export function getClassifier(handler) {
-  if (!handler) {
-    throw TypeError('handler must be specified');
+  if (typeof options.url != 'string') {
+    return next(TypeError('url must be a string'));
   }
 
-  if (typeof handler != 'function') {
-    throw TypeError('handler must be a function');
+  if (!options.classifier) {
+    return next(TypeError('classifier must be specified'));
   }
 
-  return async (req, res, next) => {
-    if (classifier) return next();
-
-    const result = handler();
-    let classifierPromise = result;
-
-    if (typeof result.then != 'function' ||
-        typeof result.catch != 'function') {
-      classifierPromise = Promise.resolve(classifierPromise);
-    }
-
-    let classifier;
-
-    try {
-      classifier = await classifierPromise();
-    }
-    catch (err) {
-      return next(err);
-    }
-
-    if (!(classifier instanceof natural.BayesClassifier)) {
-      return next(TypeError([
-        'classifier getter must return either an instance of natural.BayesClassifier or',
-        'an instance of Promise which returns an instance of natural.BayesClassifier'
-      ].join()));
-    }
-  };
-}
-
-// Then, we can register the route
-export function matchLabel(url = LABEL_DEFAULT_ENDPOINT) {
-  if (typeof options.path != 'string') {
-    throw TypeError('path must be a string');
+  if (typeof options.classifier != 'function') {
+    return next(TypeError('classifier must be a function'));
   }
 
   const router = express.Router();
 
   // After classifier has been trained, register route
-  router.get(url, (req, res) => {
-    if (!classifier) {
-      res.status(400);
-      return res.send('classifier must be initialized');
-    }
-
+  router.get(url, async (req, res) => {
     const sentence = req.query.sentence;
 
     if (!sentence) {
@@ -65,18 +30,28 @@ export function matchLabel(url = LABEL_DEFAULT_ENDPOINT) {
       return res.send('sentence must be provided');
     }
 
+    let label = options.classifier(sentence);
+
+    if (!label) {
+      res.status(404);
+      return res.send('label not found');
+    }
+
+    if (typeof label.then == 'function' &&
+        typeof label.catch == 'function') {
+      label = await label;
+    }
+
+    if (typeof label != 'string') {
+      throw TypeError('label must be a string');
+    }
+
     res.status(200);
     res.setHeader('Content-Type', 'application/json');
-
-    // Classifier might not be trained for some sentences
-    try {
-      const label = classifier.classify(sentence);
-      res.send(JSON.stringify({ label }));
-    }
-    catch (err) {
-      res.send(JSON.stringify({ label: LABEL_NOT_FOUND }));
-    }
+    res.send(JSON.stringify({ label }));
   });
 
   return router;
 }
+
+export default middleware;
